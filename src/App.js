8527bcb8688c91da423f5f89a8ae8e2f49736f43
Geraldo Melo <ghsmelo@gmail.com>
2022-12-09 16:59:08 -0300

    iniciado o componente de Cadastrar Board - Incluído dependência quill-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Questoes from "./components/questoes/Questoes";
+import NavigationBar from "./components/navigationBar/NavigationBar";
+import MenuLateral from "./components/menuLateral/MenuLateral";
+import DetalhesQuestoes from "./components/detalhesQuestoes/DetalhesQuestoes";
+import CadastrarQuestoes from "./components/cadastrarQuestao/CadastrarQuestao";
+import Login from "./components/login/Login";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import { useState } from "react";
+import { ToastContainer } from "react-bootstrap";
+import BuscaQuestoes from "./buscaQuestoes/BuscaQuestoes";
+export default function App() {
+  const [form, setForm] = useState({
+    titulo: "",
+    problema: "",
+    resultadoesperado: "",
+    tags: [],
+    datacadastro: "",
+    orgao: "",
+    respostas: 0,
+    views: 0,
+    votos: 0,
+  });
+  const [login, setLogin] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <AuthContextComponent>
+        <NavigationBar setLogin={setLogin} />
+        <Routes>
+          <Route path="/login" element={<Login />}>
+            {" "}
+          </Route>
+          <Route path="/signup" element={<Signup />}>
+            {" "}
+          </Route>
+          <Route path="/perfil" element={<Perfil />}>
+            {" "}
+          </Route>
+          <Route path="/" element={<Board />}>
+            {" "}
+          </Route>
+          <Route path="/detalhes/:id" element={<DetalhesBoard />}>
+            {" "}
+          </Route>
+          <Route path="/cadastrar" element={<CadastrarBoard />}>
+            {" "}
+          </Route>
+          <Route path="/board/:busca" element={<BuscaBoard />}>
+            {" "}
+          </Route>
+          <Route path="/tags" element={<Tags />}>
+            {" "}
+          </Route>
+          <Route path="/users" element={<Users />}>
+            {" "}
+          </Route>
+          <Route path="/orgaos" element={<Orgaos />}>
+            {" "}
+          </Route>
+          <Route path="/orgaos/cadastrar" element={<CadastrarOrgao />}>
+            {" "}
+          </Route>
+          <Route path="*" element={<ErrorPage />}>
+            {" "}
+          </Route>
+        </Routes>
+      </AuthContextComponent>
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/CadastrarBoard.js
@@ -0,0 +1,125 @@
+import axios from "axios"
+import { Button, Card, Container, Form } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './NewBoard.css'
+
+export default function CadastrarBoard({ apiUrl, form, setForm }) {
+    const [value, setValue] = useState('');
+    const toolbarOptions = {toolbar: [
+        [{font: []}, { size: [ 'small', false, 'large', 'huge' ]}],
+        [{ align: [] }, 'direction' ],
+        ['bold', 'italic', 'underline','strike', 'blockquote'],
+        [{ color: [] }, { background: [] }],
+        [{ script: 'super' }, { script: 'sub' }],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image','code-block'],
+        ['clean']
+      ]}
+//   const navigate = useNavigate()
+
+//   const dataatual = new Date()
+
+//   const handleChange = (e) => {
+//     setForm({ ...form, [e.target.name]: e.target.value });
+//   }
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+//     try {
+
+//       const tags = form.tags.toUpperCase().split(" ")
+//       form.tags = [...tags]
+//       form.datacadastro = dataatual.toLocaleString("pt-BR")
+//       await axios.post(`${apiUrl}`, form);
+//       setForm({
+//         titulo: "",
+//         problema: "",
+//         resultadoesperado: "",
+//         tags: [],
+//         datacadastro: "",
+//         orgao: "",
+//         respostas: 0,
+//         views: 0,
+//         votos:0
+//       })
+//       navigate('/questoes')
+
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+  return (
+    <Container className="container-newboard">
+    <div> <h1> Colabora!</h1></div>
+    <Form className="card-detalhe">
+          <Form.Group className="mb-3" controlId="formQuestao">
+            <Form.Label>Título:</Form.Label>
+            <Form.Control
+              className="titulo-board"
+              type="text"
+            //   name="titulo"
+            //   value={form.titulo}
+            //   onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Órgão:</Form.Label>
+            <Form.Control
+              className="orgao-board"
+              type="text"
+            //   name="orgao"
+            //   value={form.orgao}
+            //   onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+                 <ReactQuill theme="snow" value={value} onChange={setValue} modules={toolbarOptions}>
+                 </ReactQuill>
+          </Form.Group>
+          <Form.Group>
+          <Form.Label>Resultado Esperado</Form.Label>
+            <Form.Control
+              className="det-board"
+              as="textarea"
+              rows={3}
+              type="text"
+            //   name="resultadoesperado"
+            //   value={form.resultadoesperado}
+            //   onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Tags:</Form.Label>
+            <Form.Control
+              className="tag-board"
+              type="text"
+            //   name="tags"
+              placeholder="Adicione as tags separadas por um espaço"
+            //   value={form.tags}
+            //   onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Button
+            type="submit"
+            variant="success"
+            className="btn-salvar"
+            // onClick={handleSubmit}
+          >
+            Salvar
+          </Button>
+          <Button
+            variant="danger"
+            className="btn-cancelar"
+            // onClick={() => navigate("/questoes")}
+          >
+            Cancelar
+          </Button>
+       </Form>
+    </Container>
+  )
+}

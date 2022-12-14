11a1babfcf4b203e2e39962021df918ab4f00745
Diego Fernandes <32878177+diego-s-fernandes@users.noreply.github.com>
2022-12-13 22:04:44 -0300

    taglist

--- src/components/tag/TagList.js
+++ src/components/tag/TagList.js
@@ -1,1 +1,40 @@
-export default function TagList(){}
+import {Card} from "react-bootstrap";
+import React, { useEffect, useState } from 'react'
+import { api } from "../../api/api.js";
+
+export default function TagList(){
+
+    const [APIData, setAPIData] = useState([]);
+    
+
+    useEffect(() => {
+        api.get(`/alltag`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
+
+
+       return(
+
+        <div className="cardlist">     
+    
+    <Card  style={{ width: '18rem' }}>      
+       <Card.Body>
+      {APIData.map((data) =>{
+        return (
+        <><Card.Title>{data.nome}</Card.Title>
+        <Card.Text> {data.descricao}</Card.Text></>  
+        )
+      })}        
+        
+    </Card.Body>
+    </Card>
+
+    </div>
+  
+
+   )
+
+}
+

d5722a7947f4d99114a4bdf1913b861b4b8d1811
Geraldo Melo <ghsmelo@gmail.com>
2022-12-14 12:50:51 -0300

    acerto no formulário do sign-in/login que nao estava ligando com o campo emailPessoal

--- src/components/users/LoginSignUp.js
+++ src/components/users/LoginSignUp.js
@@ -164,7 +164,7 @@ export default function LoginSignUp() {
               <input
                 hidden
                 name="emailPessoal"
-                value={""}
+                value={authForm.emailPessoal}
                 onChange={handleOnChange}
               />
               <input

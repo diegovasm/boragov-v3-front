42df12e9c650fb981b0585b6bdbc4c9f6f250862
Diego Dvasm <diegovasm@gmail.com>
2022-12-14 16:37:18 -0300

    Estruturando o componente OrgaoList

--- src/components/orgao/OrgaoList.js
+++ src/components/orgao/OrgaoList.js
@@ -1,51 +1,39 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { api } from "../../api/api.js";
 
 function OrgaoList() {
+  const { orgaos, setOrgaos } = useState([]);
 
-  const { orgaos, setOrgaos } = useState([])
   useEffect(() => {
-    async function fetchOrgaos() {
-      const response = await api.get("/orgao");
-      setOrgaos(response.data);
-    }
+    try {
+      const fetchOrgaos = async () => {
+        const response = await api.get("/orgao");
+        setOrgaos(response.data);
+      };
 
-    fetchOrgaos();
-  }, [setOrgaos]);
+      fetchOrgaos();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
-    <div>
-      <h1 className="m-5">Orgaos</h1>
-      {orgaos.map((orgao) => {
-        return (
-          <div
-            className="d-flex align-items-center justify-content-center"
-            key={orgao._id}
-          >
-            <div className="w-25">
-              <div className="border-2 bgc-white my-2 py-3 shadow-sm">
-                <div className="row align-items-center">
-                  <div className="col-12 col-md-4">
-                    {/* <img src={orgao.image} height="120px" alt="orgao-img" /> */}
-                  </div>
-
-                  <ul className="list-unstyled mb-0 col-12 col-md-4 text-dark-l1 text-90 text-left my-4 my-md-0">
-                    <li className="fw-bolder">{orgao.name}</li>
-
-                    <li>{orgao.contributed_by}</li>
-                  </ul>
-
-                  <div className="col-12 col-md-4 text-center">
-                    <Link to={`/orgaos/${orgao._id}`}>+Details</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <div className="cardlist">
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          {orgaos.map((data) => {
+            return (
+              <>
+                <Card.Title>{data.nome}</Card.Title>
+                <Card.Title>{data.image}</Card.Title>
+                <Card.Text> {data.localizacao}</Card.Text>
+                <Card.Text> {data.descricao}</Card.Text>
+              </>
+            );
+          })}
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- src/components/tag/TagList.js
+++ src/components/tag/TagList.js
@@ -1,48 +1,36 @@
-import {Card} from "react-bootstrap";
-import React, { useEffect, useState } from 'react'
+import { Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { api } from "../../api/api.js";
 
-export default function TagList(){
+export default function TagList() {
+  const [apiData, setApiData] = useState([]);
 
-    const [apiData, setApiData] = useState([]);
-    
-    
-    useEffect(() => {
-        
-        try {
-            const fetchGetTag = async () => {
+  useEffect(() => {
+    try {
+      const fetchGetTag = async () => {
+        const response = await api.get("/tag/alltag");
+        setApiData(response.data);
+      };
+      fetchGetTag();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-                const response = await api.get("/tag/alltag")
-                console.log(response)
-                setApiData(response.data);
-            }
-            fetchGetTag()
-        } catch (error) {
-            console.log(error)
-        }
-    }, [])
-
-
-       return(
-
-        <div className="cardlist">     
-    
-    <Card  style={{ width: '18rem' }}>      
-       <Card.Body>
-      {apiData.map((data) =>{
-        return (
-        <><Card.Title>{data.nome}</Card.Title>
-        <Card.Text> {data.descricao}</Card.Text></>  
-        )
-      })}        
-        
-    </Card.Body>
-    </Card>
-
+  return (
+    <div className="cardlist">
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          {apiData.map((data) => {
+            return (
+              <>
+                <Card.Title>{data.nome}</Card.Title>
+                <Card.Text> {data.descricao}</Card.Text>
+              </>
+            );
+          })}
+        </Card.Body>
+      </Card>
     </div>
-  
-
-   )
-
+  );
 }
-

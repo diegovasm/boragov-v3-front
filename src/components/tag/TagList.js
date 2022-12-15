aa2a24fc5588bc245d9ff641ab9137e98c16615d
seraleroc <sergio.rocha.pa@gmail.com>
2022-12-15 18:01:50 -0300

    TagList.js mod8

--- src/components/tag/TagList.js
+++ src/components/tag/TagList.js
@@ -25,6 +25,7 @@ export default function TagList() {
         justifyContent: "spaceBetween",
         gap: "1rem",
         flexWrap: "wrap",
+        marginLeft: "1rem",
       }}
     >
       {apiData.map((data) => {
